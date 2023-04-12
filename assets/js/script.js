3e2d24f3933b6e0b015734b59d03a9efc7cb32af
josielynngriffin <jelynngriffin@gmail.com>
2023-04-12 16:08:59 -0400

    Add function to retrieve and append search history to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,36 @@ let todayContainer = $('#current-date');
 todayContainer.text(currentDate.format('MMM D, YYYY'));
 let forecastDateCont = $('#forecast-date');
 let searchedInput = $('#searchedCity__input');
-
 let searchButton = $('#search__button');
-let searchHistoryButton = $('#search-history__button');
+let searchHistoryCont = $('#search-history__cont');
+//let searchHistoryButton = $('#search-history__button');
 let openWeatherAPI = '';
 let lat= '';
 let lon= '';
 let forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +'&lon=' + lon + '&appid=b55644b1672c2e922502f7ab98105758';
 //save user submission, use geocoding api
+
+//function to retrieve, create elements, and append search history to page
+function showHistory () {
+    let retrievedHistory = JSON.parse(localStorage.getItem('searchHistory'));
+    console.log('retrieved: ' + retrievedHistory);
+    for (let i=0; i < retrievedHistory.length; i++) {
+        let generatedSearchButton = document.createElement('button');
+        let searchClasses = ['btn', '']
+        $(generatedSearchButton).addClass('btn').addClass('btn-outline-success').addClass('m-2');
+        $(generatedSearchButton).attr('id', 'search-history__button');
+        $(generatedSearchButton).text(retrievedHistory[i]);
+        console.log(generatedSearchButton);
+        $(searchHistoryCont).append(generatedSearchButton);
+    }
+    /*$(retrievedHistory).each( function(){
+        let generatedSearchButton = $('<button></button>').addClass('btn-outline-success', 'btn').attr('id', 'search-history__button');
+        //$('#search-history__button')
+        generatedSearchButton.text(retrievedHistory.indexOf());
+        console.log(generatedSearchButton);
+        $(searchHistoryCont).append(generatedSearchButton);
+    });*/
+}
 
 let searchHistory = [];
 searchButton.on('click', function(event){
@@ -27,12 +49,13 @@ searchButton.on('click', function(event){
     console.log(searchedCity);
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
     console.log('sh: ' + searchHistory);
-    //function to propogate search history
+    //function to propogate search history ((will not stay here))
+    showHistory();
+    //function to search with city with geo
+   
 })
 
-
-
-
+//function to search geo
 
 
 //let geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' +searchedCity +'&limit=5&appid=b55644b1672c2e922502f7ab98105758';
